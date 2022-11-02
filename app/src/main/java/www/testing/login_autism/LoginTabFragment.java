e1b3b0d1ef3a61e25ca8d51158371264331146f3
Adarsh <adarshsahlot@gmail.com>
2022-11-02 19:01:40 +0530

    project

--- app/src/main/java/www/testing/login_autism/LoginTabFragment.java
+++ app/src/main/java/www/testing/login_autism/LoginTabFragment.java
@@ -8,11 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class LoginTabFragment extends Fragment {
--- app/src/main/java/www/testing/login_autism/SignupTabFragment.java
+++ app/src/main/java/www/testing/login_autism/SignupTabFragment.java
@@ -1,6 +1,7 @@
 package www.testing.login_autism;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,14 +9,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SignupTabFragment extends Fragment {
 
     EditText email_signup, name_signup, pass_signup;
-    Button signup;
+    public Button signup;
     float v = 0;
     private Button button;
 
@@ -32,10 +31,42 @@ public class SignupTabFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "Your Account is Created Successfully", Toast.LENGTH_SHORT).show();
+                signUpUser();
             }
         });
-
         return root;
+    }
+
+    public void signUpUser() {
+        String name = name_signup.getText().toString().trim();
+        String email = email_signup.getText().toString().trim();
+        String password = pass_signup.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            name_signup.setError("This field is required");
+            name_signup.requestFocus();
+            return;
+        }
+        if (email.isEmpty()) {
+            email_signup.setError("This field is required");
+            email_signup.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            email_signup.setError("Please enter a valid email");
+            email_signup.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            pass_signup.setError("This field is required");
+            pass_signup.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            pass_signup.setError("The password length should be more than 6 characters");
+            pass_signup.requestFocus();
+            return;
+        }
+        Toast.makeText(getActivity(), "Your Account is Created Successfully", Toast.LENGTH_SHORT).show();
     }
 }
